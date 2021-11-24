639fe03de6835a1ca6f2e6ae2c0ef146367b7367
German <gnrg_74@hotmail.com>
2021-11-24 16:33:10 -0600

    se agrego ruta para primera version

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
+const express = require('express');
 const productRouter = require('./products');
 const usersRouter = require('./users');
 const categoriesRouter =  require('./categories');
 
 function routerApi(app){
+  const router = express.Router(); //crear ruta mestra para las diferentes verisones
+  app.use('/api/v1', router); //ruta global para todos los endpoints
   app.use('/products', productRouter);
   app.use('/categories', categoriesRouter);
   app.use('/users',  usersRouter);
