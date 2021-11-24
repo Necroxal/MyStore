46a9ea3cc14fe077b74dcffa5459500311e078e1
German <gnrg_74@hotmail.com>
2021-11-24 17:10:36 -0600

    se garego metodo patch y delete

--- routes/products.js
+++ routes/products.js
@@ -44,5 +44,23 @@ router.post('/', (req,res) => {
   });
 });
 
+router.patch('/:id', (req,res) => {
+  const {id} = req.params;
+  const body =  req.body;
+  res.json({
+    message: 'update ',
+    data: body,
+    id
+  });
+});
+
+router.delete('/:id', (req,res) => {
+  const {id} = req.params;
+  res.json({
+    message: 'Delete ',
+    id
+  });
+});
+
 module.exports = router;
 
