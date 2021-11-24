994bc97bf90bebaf0616045724d6c19c0ef74230
German <gnrg_74@hotmail.com>
2021-11-24 17:25:52 -0600

    se crearon status code

--- routes/products.js
+++ routes/products.js
@@ -28,25 +28,33 @@ router.get('/filter', (req, res) => {
 router.get('/:id', (req, res) => {
   const {
     id
-  } = req.params.id;
-  res.json({
-    id,
-    name: "Producto 2",
-    price: 800
-  })
+  } = req.params;
+  if (id == '999') {
+    res.status(404).json({
+      message: 'Not found'
+    });
+  } else {
+    res.status(200).json({
+      id,
+      name: "Producto 2",
+      price: 800
+    });
+  }
 });
 
-router.post('/', (req,res) => {
-  const body =  req.body;
-  res.json({
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
     message: 'created',
     data: body
   });
 });
 
-router.patch('/:id', (req,res) => {
-  const {id} = req.params;
-  const body =  req.body;
+router.patch('/:id', (req, res) => {
+  const {
+    id
+  } = req.params;
+  const body = req.body;
   res.json({
     message: 'update ',
     data: body,
@@ -54,8 +62,10 @@ router.patch('/:id', (req,res) => {
   });
 });
 
-router.delete('/:id', (req,res) => {
-  const {id} = req.params;
+router.delete('/:id', (req, res) => {
+  const {
+    id
+  } = req.params;
   res.json({
     message: 'Delete ',
     id
@@ -63,4 +73,3 @@ router.delete('/:id', (req,res) => {
 });
 
 module.exports = router;
-
